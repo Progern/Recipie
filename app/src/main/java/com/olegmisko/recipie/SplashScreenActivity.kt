809080fe435262a21366262a9f992ec3c3aa1ae4
Oleg <olegmiskoo@gmail.com>
2017-04-03 12:45:15 +0300

    Splash screen fully created
    [ADD] :
    
    - Icons
    - Background image
    - Buttons
    - Buttons customization
    - Theme colors
    - String resources

--- app/src/main/java/com/olegmisko/recipie/SplashScreenActivity.kt
+++ app/src/main/java/com/olegmisko/recipie/SplashScreenActivity.kt
@@ -1,12 +1,22 @@
 package com.olegmisko.recipie
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideStatusBar()
         setContentView(R.layout.activity_splash_screen)
+    }
+
+    private fun hideStatusBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
