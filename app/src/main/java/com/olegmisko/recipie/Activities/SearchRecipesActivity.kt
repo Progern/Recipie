f476d24141039b525f13076ec3237173ca2c6509
Oleg <olegmiskoo@gmail.com>
2017-04-09 22:03:18 +0300

    Hotfix commit

--- app/src/main/java/com/olegmisko/recipie/Activities/SearchRecipesActivity.kt
+++ app/src/main/java/com/olegmisko/recipie/Activities/SearchRecipesActivity.kt
@@ -4,13 +4,12 @@ import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.daimajia.androidanimations.library.Techniques
-import com.daimajia.androidanimations.library.YoYo
 import com.olegmisko.recipie.Models.Recipe
 import com.olegmisko.recipie.Models.Response
 import com.olegmisko.recipie.R
 import com.olegmisko.recipie.Services.RecipesRetrieveService
 import com.olegmisko.recipie.Views.RecipeAdapter
+import com.yarolegovich.lovelydialog.LovelyTextInputDialog
 import kotlinx.android.synthetic.main.activity_search_recipes.*
 import kotlinx.android.synthetic.main.activity_search_recipes.view.*
 import org.jetbrains.anko.indeterminateProgressDialog
@@ -33,47 +32,21 @@ class SearchRecipesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search_recipes)
         progressDialog = indeterminateProgressDialog("Fetching data…")
         progressDialog.dismiss()
-        /*
 
-        recipesTestList = ArrayList<Recipe>()
-        recipesTestList.add(Recipe("Sushi", "http://oyakis.com/wp-content/uploads/2015/06/maki-sushi-roll1.png"))
-        recipesTestList.add(Recipe("Soup", "http://thewoksoflife.com/wp-content/uploads/2015/10/egg-drop-soup-8.jpg"))
-        recipesTestList.add(Recipe("Steak", "https://www.kingsford.com/wp-content/uploads/2014/11/kfd-hoetosteak-Steak_4_0323-1024x621.jpg"))
 
-        */
-
-        /*
-        recipesAdapter = RecipeAdapter()
-        val mLayoutManager = LinearLayoutManager(this)
-        mLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        activity_search_recipes.recipesRecyclerView.adapter = recipesAdapter
-        activity_search_recipes.recipesRecyclerView.layoutManager = mLayoutManager */
-
-        activity_search_recipes.search_recipes_button.onClick {
-            if (!activity_search_recipes.recipe_search_field.text.toString().trim().isEmpty()) {
-                performSearchRequest(activity_search_recipes.recipe_search_field.text.toString().trim())
-                progressDialog.show()
-            } else {
-                YoYo.with(Techniques.Shake)
-                        .duration(500)
-                        .playOn(activity_search_recipes.recipe_search_field)
-                toast("Please fill in search query.")
-            }
+        activity_search_recipes.fab_search.onClick {
+            showInputSearchQueryDialog()
         }
-
-
     }
 
     private fun performSearchRequest(query: String) {
         val callResponse = recipesRetrieveService.getRecipes(query)
         callResponse.enqueue(object : Callback<Response> {
             override fun onFailure(call: Call<Response>?, t: Throwable?) {
-                toast("Error")
 
             }
 
             override fun onResponse(call: Call<Response>?, response: retrofit2.Response<Response>?) {
-                toast("Success")
                 if (response != null) {
                     val recipesList = ArrayList<Recipe>()
                     response.body().hits.mapTo(recipesList) { it.recipe }
@@ -88,5 +61,16 @@ class SearchRecipesActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun showInputSearchQueryDialog() {
+        LovelyTextInputDialog(this)
+                .setTopColorRes(R.color.colorPrimary)
+                .setTitle("Delicious food is coming!")
+                .setMessage("Input desirable recipe name and start your tasty journey")
+                .setIcon(R.drawable.ic_recipes_book)
+                .setConfirmButton("Search", { text ->
+                    if (!text.isEmpty()) performSearchRequest(text) })
+                .show()
     }
 }
